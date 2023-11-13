6f183e05eabd165dbf161b67bc5e12c62838fd46
Isabella <118121279+isabella-ch@users.noreply.github.com>
2023-11-13 00:27:23 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor : ['#ccc', '#fc6c7c', '#fec304', '#fc6c7c'],
+    navigation: true,
+    slidesNavigation:true,
+    navigationTooltips: ['first', 'second']
+}); 
